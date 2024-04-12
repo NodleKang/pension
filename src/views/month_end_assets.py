6ed3cb04a17e63de897ca47ac13d2511b5b8307f
Nodle Kang <nodlekang@gmail.com>
2024-04-12 12:02:35 +0900

    - multi select 추가

--- src/views/month_end_assets.py
+++ src/views/month_end_assets.py
@@ -23,13 +23,23 @@ def view_month_end_assets(config: Config, repository: DataRepository):
     # Header
     st.header("월말 자산")
 
-    tableCol, chartCol = st.columns((1, 1))
+    #
+    yyyymmCol, columnCol = st.columns(2)
+    with yyyymmCol:
+        yyyymm_list = [i for i in month_end_assets['연월'].unique()]
+        selected_yyyymms = st.multiselect("연월을 선택하세요:", yyyymm_list)
+    with columnCol:
+        column_list = month_end_assets.columns.tolist()
+        selected_columns = st.multiselect("그룹핑 컬럼을 선택하세요:", column_list)
+
+    tableCol, chartCol = st.columns(2)
 
     with tableCol:
+        st.container()  # 첫 번째 하단 컨테이너
         st.dataframe(month_end_assets)
 
     with chartCol:
+        st.container()  # 첫 번째 하단 컨테이너
         # pie 차트 작성
         pie_data = px.pie(month_end_assets, names='상품성격', values='평가액', title='상품성격별 평가액 비중')
-
         st.plotly_chart(pie_data)
