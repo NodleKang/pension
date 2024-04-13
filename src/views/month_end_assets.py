04975d3fdb291ef711a0811f3b442b36f71feb39
Nodle Kang <nodlekang@gmail.com>
2024-04-13 11:34:29 +0900

    - 평가액 총합 정보 추가

--- src/views/month_end_assets.py
+++ src/views/month_end_assets.py
@@ -1,31 +1,42 @@
+import pandas as pd
 import streamlit as st
 import plotly.express as px
+import plotly.figure_factory as ff
 
 
 MONTH_END_ASSETS = "month_end_assets"
 
 
 def view_month_end_assets(all_df: dict):
-    df = all_df[MONTH_END_ASSETS]
+    df : pd.DataFrame = all_df[MONTH_END_ASSETS]
 
     st.header("월말 자산")
 
-    yyyymmCol, columnCol = st.columns(2)
-    with yyyymmCol:
+    ####
+    summary_col = st.columns(1)
+    with summary_col[0]:
+        total_value = df['평가액'].sum()
+        print(total_value)
+        formatted_total_value = f"{total_value:,}원"
+        st.write(f'현재 자산 총액은 {formatted_total_value} 입니다.')
+
+    ####
+    yyyymm_col, column_col = st.columns(2)
+    with yyyymm_col:
         yyyymm_list = [i for i in df['연월'].unique()]
         selected_yyyymms = st.multiselect("연월을 선택하세요:", yyyymm_list)
-    with columnCol:
+    with column_col:
         column_list = df.columns.tolist()
         selected_columns = st.multiselect("그룹핑 컬럼을 선택하세요:", column_list)
 
-    tableCol, chartCol = st.columns(2)
-
-    with tableCol:
+    ####
+    table_col, chart_col = st.columns(2)
+    with table_col:
         st.container()  # 첫 번째 하단 컨테이너
         st.dataframe(df)
 
-    with chartCol:
+    with chart_col:
         st.container()  # 첫 번째 하단 컨테이너
         # pie 차트 작성
-        pie_data = px.pie(df, names='상품성격', values='평가액', title='상품성격별 평가액 비중')
-        st.plotly_chart(pie_data)
+        fig = px.pie(df, names='연금여부', values='평가액', title='평가액 비중')
+        st.plotly_chart(fig)
