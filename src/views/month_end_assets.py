285bc04d8a83546db5b4ce39a32918689e1e9e2a
Nodle Kang <nodlekang@gmail.com>
2024-04-13 11:55:02 +0900

    - 트리맵 추가

--- src/views/month_end_assets.py
+++ src/views/month_end_assets.py
@@ -12,31 +12,34 @@ def view_month_end_assets(all_df: dict):
 
     st.header("월말 자산")
 
-    ####
-    summary_col = st.columns(1)
-    with summary_col[0]:
+    # 첫번째 행
+    col1 = st.columns(1)
+    with col1[0]:
         total_value = df['평가액'].sum()
         print(total_value)
         formatted_total_value = f"{total_value:,}원"
         st.write(f'현재 자산 총액은 {formatted_total_value} 입니다.')
 
-    ####
-    yyyymm_col, column_col = st.columns(2)
-    with yyyymm_col:
+    # 두번째 행
+    col2 = st.columns(2)
+    with col2[0]:
         yyyymm_list = [i for i in df['연월'].unique()]
         selected_yyyymms = st.multiselect("연월을 선택하세요:", yyyymm_list)
-    with column_col:
+    with col2[1]:
         column_list = df.columns.tolist()
         selected_columns = st.multiselect("그룹핑 컬럼을 선택하세요:", column_list)
 
-    ####
-    table_col, chart_col = st.columns(2)
-    with table_col:
-        st.container()  # 첫 번째 하단 컨테이너
-        st.dataframe(df)
-
-    with chart_col:
+    # 세번째 행
+    col3 = st.columns(1)
+    with col3[0]:
         st.container()  # 첫 번째 하단 컨테이너
         # pie 차트 작성
-        fig = px.pie(df, names='연금여부', values='평가액', title='평가액 비중')
+        fig = px.treemap(df, path=['연금여부', '계좌특징'], values='평가액', title='비중 분포')
         st.plotly_chart(fig)
+
+
+    # 네번째 행
+    col4 = st.columns(1)
+    with col4[0]:
+        st.container()  # 첫 번째 하단 컨테이너
+        st.dataframe(df)
