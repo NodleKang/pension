1f03a6a6645bff36a95660944c19670982453bc8
Nodle Kang <nodlekang@gmail.com>
2024-07-21 11:37:13 +0900

    - 주석 추가

--- src/data/cache_manager.py
+++ src/data/cache_manager.py
@@ -3,11 +3,35 @@ from functools import lru_cache, wraps
 import time
 
 class CacheManager:
+    """
+    데이터 캐싱을 관리하는 클래스
+
+    이 클래스는 키-값 쌍으로 데이터를 저장하고, 각 데이터에 대한 만료 시간을 관리합니다.
+    캐시된 데이터는 지정된 만료 시간이 지나면 무효화됩니다. (LRU 캐시를 사용)
+
+    Attributes:
+        expiration_time (int): 캐시 항목의 만료 시간(초)
+        cache (dict): 캐시된 데이터를 저장하는 딕셔너리
+    """
     def __init__(self, expiration_time: int):
+        """
+        CacheManager 인스턴스를 초기화
+
+        Args:
+            key (str): 데이터를 식별하는 고유 키
+            data (Any): 저장할 데이터
+        """
         self.expiration_time = expiration_time
         self.cache = {}
 
     def cache_data(self, key: str, data: Any) -> None:
+        """
+        주어진 키에 데이터를 캐시합니다.
+
+        Args:
+            key (str): 캐시할 데이터의 키
+            data (Any): 캐시할 데이터
+        """
         self.cache[key] = {
             'data': data,
             'timestamp': time.time()
@@ -15,6 +39,17 @@ class CacheManager:
 
     @lru_cache(maxsize=None)
     def get_cached_data(self, key: str) -> Any:
+        """
+        주어진 키에 해당하는 캐시된 데이터를 반환합니다.
+
+        이 메서드는 lru_cache 데코레이터를 사용하여 추가적인 메모리 최적화를 제공합니다.
+
+        Args:
+            key (str): 검색할 데이터의 키
+
+        Returns:
+            Any: 캐시된 데이터. 데이터가 없거나 만료된 경우 None을 반환
+        """
         cached_item = self.cache.get(key)
         if cached_item and time.time() - cached_item['timestamp'] < self.expiration_time:
             return cached_item['data']
--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -6,30 +6,66 @@ import decimal
 
 
 class DataLoader:
+    """
+    Google Sheets에서 데이터를 로드하고 처리하는 클래스입니다.
+
+    이 클래스는 Google Sheets API를 사용하여 특정 스프레드시트에서 데이터를 로드하고,
+    로드된 데이터를 pandas DataFrame으로 변환한 후 필요한 전처리를 수행합니다.
+
+    Attributes:
+        google_sheets_helper (GoogleSheetsHelper): Google Sheets API 사용을 위한 헬퍼 객체
+        spreadsheet_id (str): 데이터를 로드할 Google 스프레드시트의 ID
+        worksheets_config (Dict[str, str]): 각 워크시트의 이름을 매핑한 설정 딕셔너리
+    """
     def __init__(self,
                  google_sheets_config: Dict[str, str],
                  worksheets_config: Dict[str, str]):
+        """
+        DataLoader 클래스의 인스턴스를 초기화합니다.
+
+        Args:
+            google_sheets_config (Dict[str, str]): Google Sheets 연결 설정
+            worksheets_config (Dict[str, str]): 워크시트 이름 매핑 설정
+        """
         self.google_sheets_helper = GoogleSheetsHelper(google_sheets_config["credentials_path"])
         self.spreadsheet_id = self.google_sheets_helper.get_spreadsheet_id(google_sheets_config["spreadsheet_name"])
         self.worksheets_config = worksheets_config
 
     def load_data(self) -> Dict[str, pd.DataFrame]:
+        """
+        설정된 Google 스프레드시트에서 데이터를 로드하고 처리합니다.
+
+        Returns:
+            Dict[str, pd.DataFrame]: 처리된 데이터프레임들을 담은 딕셔너리
+        """
         accounts_df = (self.google_sheets_helper
                        .get_worksheet_as_dataframe(self.spreadsheet_id,
                                                    self.worksheets_config["accounts"]))
         month_end_assets_df = (self.google_sheets_helper
                                .get_worksheet_as_dataframe(self.spreadsheet_id,
                                                            self.worksheets_config["month_end_assets"]))
-        pension_portfolio_df = (self.google_sheets_helper
+        io_history_df = (self.google_sheets_helper
                                 .get_worksheet_as_dataframe(self.spreadsheet_id,
-                                                            self.worksheets_config["pension_portfolio"]))
+                                                            self.worksheets_config["io_history"]))
         return {
             "accounts": self._df_manufacture(accounts_df),
             "month_end_assets": self._df_manufacture(month_end_assets_df),
-            "pension_portfolio": self._df_manufacture(pension_portfolio_df)
+            "io_history": self._df_manufacture(io_history_df)
         }
 
     def _df_manufacture(self, df: pd.DataFrame):
+        """
+        데이터프레임의 특정 컬럼들을 전처리합니다.
+
+        - 금액 관련 컬럼을 decimal 타입으로 변환
+        - '연월' 컬럼을 date 타입으로 변환하고 각 달의 마지막 날로 설정
+
+        Args:
+            df (pd.DataFrame): 전처리할 데이터프레임
+
+        Returns:
+            pd.DataFrame: 전처리된 데이터프레임
+        """
         for column in df.columns:
             # '입금', '출금', '자산', '금액'으로 끝나는 필드명을 가진 컬럼의 데이터는 decimal로 변환
             if column.endswith(('입금', '출금', '자산', '금액', '가액')):
